78b16bb07f36d6cdc3ec44a47fedba7e54077d99
Leonardo Habitzreuter <leonardohabitzreuter@senior.com.br>
2019-01-23 15:23:00 -0200

    mo functions

--- funcs.hs
+++ funcs.hs
@@ -1,6 +1,12 @@
 text = "hello world"
 
 doubleAndPlus x = (if x > 100 then x else x*2) + 1
+concatenateList = [1, 2] ++ [3, 4]
+addToList = 1 : [2, 3]
+getByIndex = "Word" !! 1
+checkingEmptyList = null []
+containsInList = 4 `elem` [3,4,5,6]
+createReplicateArray = replicate 3 "someText"
 
 main :: IO ()
 main = do
